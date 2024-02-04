b741f64e1ce6b14b81c5522d41c9d732abae277a
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2024-02-04 20:23:28 +1100

    Changed Files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+int main()
+{
+    
+    printf("Hello, my name is Rama.\n");
+    printf("I'm from Auckland, New Zealand.\n");
+    printf("I enjoy long walks on the beach.\n");
+}
